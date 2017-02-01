7a58e950c7c1a0361daadf81b248af2ac539ac3c
dfaiv <faivre@gmail.com>
2017-02-01 07:16:31 -0600

    Initial commit

---
+++ TestHostDebugFull/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace TestHostDebugFull
+{
+    public class Class1
+    {
+        [Fact]
+        public void create_client()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<StartupMock>());
+            var client = server.CreateClient();
+        }
+
+        public class StartupMock : IStartup
+        {
+            public IServiceProvider ConfigureServices(IServiceCollection services)
+            {
+                return services.BuildServiceProvider();
+            }
+
+            public void Configure(IApplicationBuilder app)
+            {
+                
+            }
+        }
+    }
+}
---
+++ TestHostDebugPackageConfig/PackageConfigTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace TestHostDebugPackageConfig
+{
+    public class PackageConfigTests
+    {
+        public class StartupMock : IStartup
+        {
+            public IServiceProvider ConfigureServices(IServiceCollection services)
+            {
+                return services.BuildServiceProvider();
+            }
+
+            public void Configure(IApplicationBuilder app)
+            {
+            }
+        }
+
+        [Fact]
+        public void create_client()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<StartupMock>());
+            var client = server.CreateClient();
+        }
+    }
+}
